7ac2920158040f7718e1e0b9b3053188eadd6254
Stephen McIntosh <stephenmac7@gmail.com>
2014-11-11 18:50:13 -0500

    Removed junk

--- CodeMirror.hs
+++ CodeMirror.hs
@@ -8,16 +8,6 @@ codeMirror = do
     addStylesheet $ StaticR css_codemirror_css
     addStylesheet $ StaticR css_codemirror_custom_css
     addStylesheet $ StaticR js_addon_dialog_dialog_css
-    -- addScript $ StaticR js_codemirror_js
-    -- addScript $ StaticR js_mode_xml_js
-    -- addScript $ StaticR js_mode_css_js
-    -- addScript $ StaticR js_mode_javascript_js
-    -- addScript $ StaticR js_mode_htmlmixed_js
-    -- addScript $ StaticR js_addon_dialog_dialog_js
-    -- addScript $ StaticR js_addon_edit_trailingspace_js
-    -- addScript $ StaticR js_addon_search_search_js
-    -- addScript $ StaticR js_addon_search_match_highlighter_js
-    -- addScript $ StaticR js_addon_search_searchcursor_js
     addScript $ StaticR js_codemirror_min_js
     toWidget [julius|
       function cm_areas() {
